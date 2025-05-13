933b40a6c42f73998e4520cfa87dc2999bf22172
Mariana Garcia <mariana.garcia@student.swic.edu>
2025-05-13 15:04:21 -0500

    This is Add FetchPeople to get data from SWAPI

--- src/action.js
+++ src/action.js
@@ -1,9 +1,15 @@
 import state from './store.js'
 
-export async function fetchPeople(){
-    store.setState({ isLoading: true, error: null })
+export async function fetchPeople() {
+  store.setState({ isLoading: true, error: null })
 
-    try {
-        const res = await fetch
-    }
+  try {
+    const res = await fetch('https://www.swapi.tech/api/people')
+    const json = await res.json()
+    const people = json.result || json.results || []
+
+    store.setState({ people, isLoading: false })
+  } catch (error) {
+    store.setState({ error: error.message, isLoading: false, people: [] })
+  }
 }
